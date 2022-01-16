861beb41d02872422d4d0e6d10f1e8e22356114e
Dániel Varjaskéri <varjaskerid@gmail.com>
2022-01-16 01:18:52 +0100

    fully functions just not pretty

--- test.js
+++ test.js
@@ -10,7 +10,14 @@ function Book (title,pages,author){
 
 
 
+function read(){
+  this.style.backgroundColor = "rgb(88,228,46)";
 
+}
+
+function removeBook(){
+  this.parentElement.parentElement.remove();
+ }
 
 
 
@@ -21,16 +28,28 @@ function add (){
   const cardSlot1 = document.createElement("div");
   const cardSlot2 = document.createElement("div");
   const cardSlot3 = document.createElement("div");
+  const cardSlot4 = document.createElement("div");
+  cardSlot4.classList.add("cardSlot4");
+  const haveYReadBtn = document.createElement("button");
+  haveYReadBtn.textContent = "Read";
+  haveYReadBtn.setAttribute("id","haveYouReadBtn");
+  haveYReadBtn.addEventListener("click",read);
+  const deleteBtn = document.createElement("button");
+  deleteBtn.textContent = "DELETE";
+  deleteBtn.setAttribute("id","deleteBtn");
+  deleteBtn.addEventListener("click",removeBook);
   card.appendChild(cardSlot1);
   card.appendChild(cardSlot2);
   card.appendChild(cardSlot3);
+  card.appendChild(cardSlot4);
   let bookTitle = document.querySelector("#bookTitle").value;
   let bookPages = document.querySelector("#bookPages").value;
   let bookAuthor = document.querySelector("#bookAuthor").value;
-  console.log(bookTitle,bookPages,bookAuthor);
   cardSlot1.textContent = bookTitle;
   cardSlot2.textContent = bookPages;
   cardSlot3.textContent = bookAuthor;
+  cardSlot4.appendChild(haveYReadBtn);
+  cardSlot4.appendChild(deleteBtn);
   container.appendChild(card);
   removeActive();
 }
